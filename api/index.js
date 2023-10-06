4b240346032ee0406758c63d28bd9f6cc9b56f3a
Justin Cheng <justincheng@Justins-MBP.home>
2023-10-06 18:05:25 -0400

    created api route and controller

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from "express"; //import express so we can start setting up web server with routing and better ssr and api integration
 import mongoose from "mongoose"; //import the database
 import dotenv from "dotenv"; // imported so we can use .env file to safley store mongo token
+import userRouter from "./routes/user.route.js";
 dotenv.config();
 
 mongoose
@@ -13,5 +14,8 @@ mongoose
 const app = express();
 //installed nodemon so server updates by itselft
 app.listen(3000, () => {
-  console.log("Server is running on port 3000??");
+  console.log("Server is running on port 3000");
 });
+
+//create api route, req is wat is sent from font end, res is resp from backend
+app.use("/api/user", userRouter);
