8e2e05f3d9dabb10df8362856d6304e0ee59a8bd
Justin Cheng <justincheng@Justins-MBP.home>
2023-10-06 17:13:47 -0400

    user schema added

---
+++ api/models/user.model.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+//import mongoose so we can create object schema for how the data is structured
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
