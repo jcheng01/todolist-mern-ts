1e90e6906c0108ffb8a89c92ed68051c37c1baca
Justin Cheng <justincheng@Justins-MBP.home>
2023-10-06 16:49:40 -0400

    connected to database

--- api/index.js
+++ api/index.js
@@ -1,4 +1,14 @@
-import express from "express";
+import express from "express"; //import express so we can start setting up web server with routing and better ssr and api integration
+import mongoose from "mongoose"; //import the database
+import dotenv from "dotenv"; // imported so we can use .env file to safley store mongo token
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connected to mongo");
+  })
+  .catch((error) => console.log(error)); //chain promises to see if db is connected
 
 const app = express();
 //installed nodemon so server updates by itselft
